722aa5fe318902b066a2e07da35c7a04d199bc42
Code-f-ai <h.e.n.r.y.b.r.i.a.n.j.e.r.r.y@gmail.com>
2025-07-17 19:10:47 +0700

    Update project structure and deploy config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import path from 'path';
 
 export default defineConfig({
+  base: '/mogo-template/',
   resolve: {
     alias: {
        '@styles': path.resolve(__dirname, 'src/styles'),
